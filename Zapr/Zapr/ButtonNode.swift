c784e26b9dc55b8aa7c9086e75228c590811a3bc
jesseleeruiz <jesseleeruiz@gmail.com>
2020-09-01 11:07:38 -1000

    Added game background, logo, and slogan to menu screen. Adding custom button with focus now.

---
+++ Zapr/Zapr/ButtonNode.swift
@@ -0,0 +1,14 @@
+//
+//  ButtonNode.swift
+//  Zapr
+//
+//  Created by Jesse Ruiz on 9/1/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ButtonNode: SKSpriteNode {
+
+}
--- Zapr/Zapr/GameScene.swift
+++ Zapr/Zapr/GameScene.swift
@@ -11,4 +11,18 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.zPosition = -1
+        background.blendMode = .replace
+        addChild(background)
+        
+        let title = SKSpriteNode(imageNamed: "blastazapTitle")
+        title.position = CGPoint(x: 0, y: 200)
+        addChild(title)
+        
+        let strap = SKSpriteNode(imageNamed: "strap")
+        strap.position = CGPoint(x: 0, y: 50)
+        addChild(strap)
+    }
 }
