33ce46857544483b2e0ba367f0cd2214d30f684e
jesseleeruiz <jesseleeruiz@gmail.com>
2020-09-01 10:15:03 -1000

    Created Xcode project.

---
+++ Zapr/Zapr/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  Zapr
+//
+//  Created by Jesse Ruiz on 9/1/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
